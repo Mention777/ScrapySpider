b500b75adb58658e098657fcdce296bbaa8cf46b
huangmengxuan <huangmengxuan@cheweiguanjia.com>
2018-03-14 17:12:38 +0800

    使用item loader将item进行改写

--- testSpider/items.py
+++ testSpider/items.py
@@ -6,6 +6,12 @@
 # https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+import datetime
+import re
+
+from  scrapy.loader import  ItemLoader
+from  scrapy.loader.processors import  TakeFirst,MapCompose
+from  testSpider.Tools.common import get_MD5
 
 
 class TestspiderItem(scrapy.Item):
@@ -14,17 +20,57 @@ class TestspiderItem(scrapy.Item):
     pass
 
 
+def deal_datestr(value):
+    try:
+        create_time = datetime.datetime.strptime(value, "%Y/%m/%d").date()
+    except Exception as e:
+        create_time = datetime.datetime.now().date()
+
+    return create_time
+
+def deal_num(value):
+    r = re.findall('.*?(\d+).*', value)
+    if len(r):
+        num = int(r[0])
+    else:
+        num = 0
+    return num
+
+def return_value(value):
+    return value
+
+def return_MD5str(value):
+    return get_MD5(value)
+
+
+class ArticleItemLoader(ItemLoader):
+    default_output_processor = TakeFirst()
+
 class ArticleItem(scrapy.Item):
     title_name= scrapy.Field()
-    public_time = scrapy.Field()
-    good_num = scrapy.Field()
-    collect_num = scrapy.Field()
-    comment_num = scrapy.Field()
+    public_time = scrapy.Field(
+
+        # 对传入的值进行预处理
+        input_processor= MapCompose(deal_datestr)
+    )
+    good_num = scrapy.Field(
+        input_processor=MapCompose(deal_num)
+    )
+    collect_num = scrapy.Field(
+        input_processor = MapCompose(deal_num)
+    )
+    comment_num = scrapy.Field(
+        input_processor=MapCompose(deal_num)
+    )
     content = scrapy.Field()
     url = scrapy.Field()
-    image_url = scrapy.Field()
+    image_url = scrapy.Field(
+        output_processor=MapCompose(return_value)
+    )
 
     # 保存图片的路径
     image_path= scrapy.Field()
     # 使用md5对url进行加密
-    url_object_id = scrapy.Field()
+    url_object_id = scrapy.Field(
+        input_processor = MapCompose(return_MD5str)
+    )
--- testSpider/spiders/jobbole.py
+++ testSpider/spiders/jobbole.py
@@ -3,8 +3,9 @@ import scrapy
 import  re
 from urllib import  parse
 from scrapy.http import Request
-from testSpider.items import ArticleItem
+from testSpider.items import ArticleItem,ArticleItemLoader
 from testSpider.Tools.common import  get_MD5
+from scrapy.loader import  ItemLoader
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
@@ -38,45 +39,61 @@ class JobboleSpider(scrapy.Spider):
         '''
 
 
-        # 文章标题
-        title_name = response.xpath('//div[@class = "entry-header"]/h1/text()').extract_first("")
-
-        # 发布日期
-        public_time = response.xpath('//p[@class = "entry-meta-hide-on-mobile"]/text()').extract_first("").strip().replace('·','').strip()
-
-        # 点赞数
-        good_num = response.xpath('//*[@class =" btn-bluet-bigger href-style vote-post-up   register-user-only "]/h10/text()').extract_first("")
-        # 收藏数
-        collect_num= response.xpath('//span[contains(@data-site-id,"13")]/text()').extract_first("").strip()
-        r = re.findall('.*?(\d+).*',collect_num)
-        if len(r):
-            collect_num = int(r[0])
-        else:
-            collect_num = 0
-
-        # 评论数
-        comment_num = response.xpath('//span[@class = "btn-bluet-bigger href-style hide-on-480"]/text()').extract_first("").strip()
-        r = re.findall('.*?(\d+).*', comment_num)
-        if len(r):
-            comment_num = int(r[0])
-        else:
-            comment_num = 0
-
-        # 文章内容
-        content = response.xpath("//div[@class='entry']").extract_first("")
+        # # 文章标题
+        # title_name = response.xpath('//div[@class = "entry-header"]/h1/text()').extract_first("")
+        #
+        # # 发布日期
+        # public_time = response.xpath('//p[@class = "entry-meta-hide-on-mobile"]/text()').extract_first("").strip().replace('·','').strip()
+        #
+        # # 点赞数
+        # good_num = response.xpath('//*[@class =" btn-bluet-bigger href-style vote-post-up   register-user-only "]/h10/text()').extract_first("")
+        # # 收藏数
+        # collect_num= response.xpath('//span[contains(@data-site-id,"13")]/text()').extract_first("").strip()
+        # r = re.findall('.*?(\d+).*',collect_num)
+        # if len(r):
+        #     collect_num = int(r[0])
+        # else:
+        #     collect_num = 0
+        #
+        # # 评论数
+        # comment_num = response.xpath('//span[@class = "btn-bluet-bigger href-style hide-on-480"]/text()').extract_first("").strip()
+        # r = re.findall('.*?(\d+).*', comment_num)
+        # if len(r):
+        #     comment_num = int(r[0])
+        # else:
+        #     comment_num = 0
+        #
+        # # 文章内容
+        # content = response.xpath("//div[@class='entry']").extract_first("")
+        #
+        # # 封面图
+        # front_image_url = response.meta.get("front_image_url","")
+        #
+        # aritcle_item = ArticleItem()
+        # aritcle_item['title_name'] = title_name
+        # aritcle_item['public_time'] = public_time
+        # aritcle_item['good_num'] = good_num
+        # aritcle_item['collect_num'] = collect_num
+        # aritcle_item['comment_num'] = comment_num
+        # aritcle_item['content'] = content
+        # aritcle_item['url'] = response.url
+        # aritcle_item['image_url'] = [front_image_url]
+        # aritcle_item['url_object_id'] = get_MD5(response.url)
 
         # 封面图
-        front_image_url = response.meta.get("front_image_url","")
+        front_image_url = response.meta.get("front_image_url", "")
+        item_loader = ArticleItemLoader(item=ArticleItem(), response=response)
+        item_loader.add_xpath("title_name", '//div[@class = "entry-header"]/h1/text()')
+        item_loader.add_xpath("public_time", '//p[@class = "entry-meta-hide-on-mobile"]/text()')
+        item_loader.add_xpath("good_num",
+                              '//*[@class =" btn-bluet-bigger href-style vote-post-up   register-user-only "]/h10/text()')
+        item_loader.add_xpath("collect_num", '//span[contains(@data-site-id,"13")]/text()')
+        item_loader.add_xpath("comment_num", '//span[@class = "btn-bluet-bigger href-style hide-on-480"]/text()')
+        item_loader.add_xpath("content", '//span[@class = "btn-bluet-bigger href-style hide-on-480"]/text()')
+        item_loader.add_value("url", response.url)
+        item_loader.add_value("image_url", [front_image_url])
+        item_loader.add_value("url_object_id", response.url)
 
-        aritcle_item = ArticleItem()
-        aritcle_item['title_name'] = title_name
-        aritcle_item['public_time'] = public_time
-        aritcle_item['good_num'] = good_num
-        aritcle_item['collect_num'] = collect_num
-        aritcle_item['comment_num'] = comment_num
-        aritcle_item['content'] = content
-        aritcle_item['url'] = response.url
-        aritcle_item['image_url'] = [front_image_url]
-        aritcle_item['url_object_id'] = get_MD5(response.url)
+        aritcle_item = item_loader.load_item()
 
         yield  aritcle_item
